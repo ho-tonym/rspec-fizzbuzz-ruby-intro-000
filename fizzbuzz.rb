de34f99c097b4e8c7edc92cd5c16d6f2d1ae705e
Tony Ho <ho.tonym@gmail.com>
2018-04-24 00:42:05 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,11 +4,11 @@ require_relative '../fizzbuzz.rb'
 def fizzbuzz(number)
 
   if number%3 == 0
-    puts "Fizz"
+    return "Fizz"
   elsif number%5 == 0
-    puts "Buzz"
+    return "Buzz"
   elsif (number%5 == 0) && (number%3 == 0)
-    puts "FizzBuzz"
+    return "FizzBuzz"
   else
     return nil
   end
