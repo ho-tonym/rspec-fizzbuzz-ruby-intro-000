dbd6d2c2c88a98004938eb20a32953f5316355bb
Tony Ho <ho.tonym@gmail.com>
2018-04-24 00:34:38 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,14 @@
+def fizzbuzz(number)
+
+  if number%3 == 0
+    puts "Fizz"
+  elsif number%5 == 0
+    puts "Buzz"
+  elsif (number%5 == 0) && (number%3 == 0)
+    puts "FizzBuzz"
+  else
+    return nil
+  end
+    
+
+end
